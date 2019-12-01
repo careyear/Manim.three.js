ec67ec043789d46be883eae42c990308df30f6ad
ista2000 <mishra.istasis@gmail.com>
2019-12-01 20:19:20 +0530

    Added texture and orbit controls

--- index.js
+++ index.js
@@ -1,91 +1,150 @@
 import {
-    Mesh,
     Scene,
     Color,
     PerspectiveCamera,
+    HemisphereLight,
+    DirectionalLight,
     BoxBufferGeometry,
+    TextureLoader,
+    sRGBEncoding,
     MeshStandardMaterial,
-    WebGLRenderer,
-    DirectionalLight
+    Mesh,
+    WebGLRenderer
 } from './node_modules/three/src/Three.js';
-
+import {OrbitControls} from './node_modules/three/examples/jsm/controls/OrbitControls.js';
 // these need to be accessed inside more than one function so we'll declare them first
 let container;
 let camera;
 let renderer;
 let scene;
 let mesh;
-let light;
+let controls;
 
 function init() {
 
-    // Get a reference to the container element that will hold our scene
-    container = document.querySelector( '#scene-container' );
+  container = document.querySelector( '#scene-container' );
 
-    // create a Scene
-    scene = new Scene();
+  scene = new Scene();
+  scene.background = new Color( 0x8FBCD4 );
 
-    scene.background = new Color( 0x8FBCD4 );
+  createCamera();
+  createControls();
+  createLights();
+  createMeshes();
+  createRenderer();
 
-    // set up the options for a perspective camera
-    const fov = 35; // fov = Field Of View
-    const aspect = container.clientWidth / container.clientHeight;
+  renderer.setAnimationLoop( () => {
 
-    const near = 0.1;
-    const far = 100;
+    update();
+    render();
 
-    camera = new PerspectiveCamera( fov, aspect, near, far );
+  } );
 
-    // every object is initially created at ( 0, 0, 0 )
-    // we'll move the camera back a bit so that we can view the scene
-    camera.position.set( 0, 0, 10 );
+}
 
-    // create a geometry
-    const geometry = new BoxBufferGeometry( 2, 2, 2 );
+function createCamera() {
 
-    // create a default (white) Basic material
-    const material = new MeshStandardMaterial( { color: 0x800080 } );
+  camera = new PerspectiveCamera(
+    35, // FOV
+    container.clientWidth / container.clientHeight, // aspect
 
-    // create a Mesh containing the geometry and material
-    mesh = new Mesh( geometry, material );
+    0.1, // near clipping plane
+    100, // far clipping plane
+  );
 
-    // add the mesh to the scene object
-    scene.add( mesh );
+  camera.position.set( -4, 4, 10 );
 
-    // Create a directional light
-    light = new DirectionalLight( 0xffffff, 5.0 );
+}
+function createControls() {
 
-    // move the light back and up a bit
-    light.position.set( 10, 10, 10 );
+  controls = new OrbitControls( camera, container );
 
-    // remember to add the light to the scene
-    scene.add( light );
+}
 
-    // create a WebGLRenderer and set its width and height
-    renderer = new WebGLRenderer({antialias: true});
+function createLights() {
+
+    const ambientLight = new HemisphereLight(
+    0xddeeff, // bright sky color
+    0x202020, // dim ground color
+    5, // intensity
+    );
+    scene.add( ambientLight );
+
+    const mainLight = new DirectionalLight( 0xffffff, 5);
+    mainLight.position.set( 10, 10, 10 );
+
+    scene.add( ambientLight, mainLight );
+
+}
+
+function createMeshes() {
+
+  const geometry = new BoxBufferGeometry( 2, 2, 2 );
+
+  const textureLoader = new TextureLoader();
+
+  const texture = textureLoader.load( 'textures/uv_test_bw.png' );
+
+  texture.encoding = sRGBEncoding;
+  texture.anisotropy = 16;
+
+  const material = new MeshStandardMaterial( {
+    map: texture,
+  } );
+
+  mesh = new Mesh( geometry, material );
+
+  scene.add( mesh );
+
+}
+
+function createRenderer() {
+
+  renderer = new WebGLRenderer( { antialias: true } );
+  renderer.setSize( container.clientWidth, container.clientHeight );
+
+  renderer.setPixelRatio( window.devicePixelRatio );
+
+  renderer.gammaFactor = 2.2;
+  renderer.gammaOutput = true;
+  renderer.physicallyCorrectLights = true;
+
+  container.appendChild( renderer.domElement );
+
+}
+
+function update() {
+    // increase the mesh's rotation each frame
+    // mesh.rotation.z += 0.01;
+    // mesh.rotation.x += 0.01;
+    // mesh.rotation.y += 0.01;
+}
+
+function render() {
+    renderer.render( scene, camera );
+}
+function play() {
+    renderer.setAnimationLoop(() => {
+        update();
+        render();
+    });
+}
+function stop() {
+    renderer.setAnimationLoop(null);
+}
+window.addEventListener('resize', () => {
+    // set the aspect ratio to match the new browser window aspect ratio
+    camera.aspect = container.clientWidth / container.clientHeight;
+
+
+    // update the camera's frustum
+    camera.updateProjectionMatrix();
+
+    // update the size of the renderer AND the canvas
     renderer.setSize( container.clientWidth, container.clientHeight );
 
-    renderer.setPixelRatio( window.devicePixelRatio );
-
-    // add the automatically created <canvas> element to the page
-    container.appendChild( renderer.domElement );
-}
-
-function animate() {
-  // render, or 'create a still image', of the scene
-    requestAnimationFrame(animate);
-
-    // increase the mesh's rotation each frame
-    mesh.rotation.z += 0.01;
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.01;
-
-    renderer.render( scene, camera );
-
-}
+});
 
 // call the init function to set everything up
 init();
-
-// then call the animate function to render the scene
-animate();
+play();
